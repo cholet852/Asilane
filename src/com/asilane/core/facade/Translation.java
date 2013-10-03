beab04aa20d26de9dabd5d665cdfee892f985baa
Walane <walane@walane.net>
2013-10-04 00:07:12 +0200

    Update Translation for the new architecture

--- src/com/asilane/core/facade/Translation.java
+++ src/com/asilane/core/facade/Translation.java
@@ -37,6 +37,25 @@ public class Translation {
 	}
 
 	/**
+	 * 
+	 * @param key
+	 * @param vars
+	 * @return The answer corresponding to the translation key<br>
+	 *         Example : <code>getAnswer("prety_cloudy", "Paris")</code> :<br>
+	 *         "It's pretty cloudy in {0}" -> "It's pretty cloudy in Paris"
+	 */
+	public String getAnswer(final String key, final String... vars) {
+		String response = String.valueOf(translationMap.get(key + ANSWER));
+
+		// Parse {n}
+		for (int i = 0; i < vars.length; i++) {
+			response = response.replace("{" + i + "}", vars[i]);
+		}
+
+		return response;
+	}
+
+	/**
 	 * @see Properties.values()
 	 */
 	public Collection<Object> values() {
