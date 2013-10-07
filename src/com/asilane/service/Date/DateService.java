f0064d7daf04ac260c292fe419e89a4d00a253f8
Walane <walane@walane.net>
2013-10-07 21:35:04 +0200

    Update AsilaneDateService for the new architecture

---
+++ src/com/asilane/service/Date/DateService.java
@@ -0,0 +1,70 @@
+package com.asilane.service.Date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.asilane.core.AsilaneUtils;
+import com.asilane.core.facade.Question;
+import com.asilane.core.facade.Response;
+import com.asilane.core.facade.ServiceDispatcher;
+import com.asilane.core.facade.Translator;
+import com.asilane.core.facade.history.HistoryNode;
+import com.asilane.service.IService;
+
+/**
+ * @author walane
+ * 
+ */
+public class DateService implements IService {
+
+	private static enum dynamicCommands {
+		what_is_the_date, what_time_is_it, and_now
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.asilane.service.Service#handleService(java.lang.String, com.asilane.recognition.Locale)
+	 */
+	@Override
+	public Response handleService(final Question question) {
+		final Translator translator = ServiceDispatcher.getInstance(question.getLanguage()).getTranslation(this);
+
+		final Date date = new Date();
+		final DateFormat dateFormat = new SimpleDateFormat("EEEE d MMMM YYYY");
+		final DateFormat hourFormat = new SimpleDateFormat("HH:mm");
+
+		if (AsilaneUtils.extractRegexVars(translator.getQuestion(dynamicCommands.what_is_the_date), question) != null) {
+			return new Response(translator.getAnswer(dynamicCommands.what_is_the_date, dateFormat.format(date)));
+		} else if (AsilaneUtils.extractRegexVars(translator.getQuestion(dynamicCommands.what_time_is_it), question) != null) {
+			return new Response(translator.getAnswer(dynamicCommands.what_time_is_it, hourFormat.format(date)));
+		}
+
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.asilane.service.IService#handleRecoveryService(java.lang.String, com.asilane.core.Locale)
+	 */
+	@Override
+	public Response handleRecoveryService(final Question question) {
+		final Translator translator = ServiceDispatcher.getInstance(question.getLanguage()).getTranslation(this);
+
+		if (AsilaneUtils.extractRegexVars(translator.getQuestion(dynamicCommands.and_now), question) != null) {
+			// If the last question concern this service, return directly the last answer
+			final HistoryNode lastRequest = question.getHistoryTree().getLastNode();
+			if (lastRequest.getService().equals(this)) {
+				return lastRequest.getResponse();
+			}
+
+			// Else, return another answer
+			final DateFormat dateFormat = new SimpleDateFormat("EEEE d MMMM YYYY");
+			return new Response(translator.getAnswer(dynamicCommands.what_is_the_date, dateFormat.format(new Date())));
+		}
+
+		return null;
+	}
+}
