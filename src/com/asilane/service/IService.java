c327a5afad45164256bf66f3c52c02cf39f12216
Walane <walane@walane.net>
2013-10-07 10:47:37 +0200

    New architecture

--- src/com/asilane/service/IService.java
+++ src/com/asilane/service/IService.java
@@ -1,9 +1,7 @@
 package com.asilane.service;
 
-import java.util.Locale;
-import java.util.Set;
-
-import com.asilane.core.facade.history.HistoryTree;
+import com.asilane.core.facade.Question;
+import com.asilane.core.facade.Response;
 
 /**
  * Each Service have to implemente this interface to be used on the application
@@ -14,29 +12,18 @@ public interface IService {
 	/**
 	 * This will return an appropriate answer to the question in the good Locale
 	 * 
-	 * @param sentence
-	 * @param historyTree
-	 * @param Locale
+	 * @param question
+	 * 
 	 * @return an appropriate answer to the question in the good Locale
 	 */
-	String handleService(final String sentence, final Locale lang, final HistoryTree historyTree);
-
-	/**
-	 * This will returns all sentences which the service can be called
-	 * 
-	 * @param lang
-	 * @return all sentences which the service can be called
-	 */
-	Set<String> getCommands(final Locale lang);
+	Response handleService(final Question question);
 
 	/**
 	 * This will return an appropriate answer if there is no answer given
 	 * 
-	 * @param sentence
-	 * @param historyTree
-	 *            TODO
-	 * @param Locale
+	 * @param question
+	 * 
 	 * @return an appropriate answer if there is no answer given
 	 */
-	String handleRecoveryService(final String sentence, final Locale lang, final HistoryTree historyTree);
+	Response handleRecoveryService(final Question question);
 }
